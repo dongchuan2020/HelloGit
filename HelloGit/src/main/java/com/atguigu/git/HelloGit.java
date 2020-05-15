a1abbf6e6a0692fd4fd32fbafd9f82a78e556e94
dongchuan <403222606@qq.com>
2020-05-15 20:57:14 +0800

    更新4

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -4,7 +4,5 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("更新1");
-        System.out.println("更新2");
-        System.out.println("更新3");
     }
 }
